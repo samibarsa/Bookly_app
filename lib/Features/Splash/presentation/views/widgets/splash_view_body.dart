0b1b00cfe6550e6ce0140c0c033991d01ee45c06
samibarsa <sami5114943@gmail.com>
2023-08-14 19:23:31 +0300

    animation done

--- lib/Features/Splash/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/Splash/presentation/views/widgets/splash_view_body.dart
@@ -1,11 +1,45 @@
-import 'package:bookly_app/utils/assets.dart';
+import 'package:bookly_app/Features/Splash/presentation/views/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
 
-class SplachViewBody extends StatelessWidget {
+import '../../../../../core/assets.dart';
+
+class SplachViewBody extends StatefulWidget {
   const SplachViewBody({super.key});
 
   @override
+  State<SplachViewBody> createState() => _SplachViewBodyState();
+}
+
+class _SplachViewBodyState extends State<SplachViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidinganimation;
+  @override
+  void initState() {
+    super.initState();
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+    slidinganimation =
+        Tween<Offset>(begin: const Offset(0, 5), end: Offset.zero)
+            .animate(animationController);
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Center(child: Image.asset(AssetesData.logo));
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Image.asset(AssetesData.logo),
+        SlidingText(slidinganimation: slidinganimation)
+      ],
+    );
   }
 }
